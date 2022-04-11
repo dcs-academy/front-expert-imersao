5d3db1059fb40aa3ee1ff87f8a1ef0605865b998
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-11 03:03:24 -0300

    feat: adicionando componente header e footer

---
+++ front-on-food/components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>Imersão FrontExpert</p>
+    </footer>
+  );
+}
---
+++ front-on-food/components/header/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
