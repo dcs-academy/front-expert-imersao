ed9df7f6d6af65ee0f4cf4d4cbe63bcf72c13ae6
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-11 03:18:08 -0300

    feat: adicionado fonte do projeto

--- front-on-food/tailwind.config.js
+++ front-on-food/tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
