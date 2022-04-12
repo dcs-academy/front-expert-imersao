496d78608defb58f4dadcc688844e5abaa34e8a1
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-12 10:18:50 -0300

    feat: adicionad images, icons e suporte a svg

---
+++ front-on-food/icons/index.ts
@@ -0,0 +1,19 @@
+import IconBookmark from "../public/images/icons/bookmark.svg";
+import IconHome from "../public/images/icons/home.svg";
+import IconDelivery from "../public/images/icons/delivery.svg";
+import IconCategory from "../public/images/icons/category.svg";
+import IconFilter from "../public/images/icons/filter.svg";
+import IconHeart from "../public/images/icons/heart.svg";
+import IconReviews from "../public/images/icons/reviews.svg";
+import IconRestaurant from "../public/images/icons/restaurant.svg";
+
+export {
+  IconBookmark,
+  IconHome,
+  IconDelivery,
+  IconCategory,
+  IconFilter,
+  IconHeart,
+  IconReviews,
+  IconRestaurant,
+};
