2f5fd47dafa87278edfbeb2c72c1ae0c7976c642
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-12 10:18:02 -0300

    feat: atualizado footer

--- front-on-food/components/footer/footer.tsx
+++ front-on-food/components/footer/footer.tsx
@@ -1,7 +1,11 @@
+import Styles from "./footer.module.css";
+
 export function Footer() {
   return (
     <footer>
-      <p>Imersão FrontExpert</p>
+      <div className={Styles.footer}>
+        <p>@ 2022 - Imersão FrontExpert</p>
+      </div>
     </footer>
   );
 }
