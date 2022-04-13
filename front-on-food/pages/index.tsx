26f4a8686b268108f4458e862ed5200fb3019541
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-12 23:32:01 -0300

    feat: atualizado pagina inicial

--- front-on-food/pages/index.tsx
+++ front-on-food/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { PageTemplate, Search, Dishe } from "@/components";
+import { Character, PageTemplate, Search } from "@/components";
+import Styles from "../styles/internals.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -12,12 +13,14 @@ const Home: NextPage = () => {
       </Head>
 
       <PageTemplate>
-        <h1>Encontre opções em Delivery próximos à você.</h1>
-        <Search />
-        <h2>
-          Descubra seu novo Delivery favorito. Milhares de opções a um clique.
-        </h2>
-        <Dishe />
+        <div className={Styles.content}>
+          <h1>Encontre opções em Delivery próximos à você.</h1>
+          <Search />
+          <h2>
+            Descubra seu novo Delivery favorito. Milhares de opções a um clique.
+          </h2>
+          <Character />
+        </div>
       </PageTemplate>
     </>
   );
