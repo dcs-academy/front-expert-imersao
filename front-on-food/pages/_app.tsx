84f1de0eeaa4d031d9525416e1effed454ec203e
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-15 17:31:17 -0300

    fix: atualizado dependencias do projeto

--- front-on-food/pages/_app.tsx
+++ front-on-food/pages/_app.tsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
