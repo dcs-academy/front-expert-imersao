a6a6b6b4aa2d1cf90d0a030d2f5dfa5fca9d72b7
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-16 23:34:55 -0300

    feat: atualizado titulos e _document.tsx

--- front-on-food/pages/descobrir/[city].tsx
+++ front-on-food/pages/descobrir/[city].tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import Head from "next/head";
 import { PageTemplate, DishList } from "@/components";
 import Styles from "../../styles/discovery.module.css";
 import { api } from "@/services";
@@ -8,15 +8,25 @@ export default function Descobrir(props: PageDiscoverProps) {
   const { city } = props;
 
   return (
-    <PageTemplate>
-      <div className={Styles.content}>
-        <h1>Opções na região de {city.name}</h1>
-        <p>Encontramos {city.catalogEstimated} opções</p>
-        <div className={Styles.items}>
-          <DishList citySlug={city.slug} />
+    <>
+      <Head>
+        <title>Opções em {city.name} - OnFood App</title>
+        <meta
+          name="description"
+          content={`Encontre opções em Delivery próximos à você em ${city.name}`}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PageTemplate>
+        <div className={Styles.content}>
+          <h1>Opções na região de {city.name}</h1>
+          <p>Encontramos {city.catalogEstimated} opções</p>
+          <div className={Styles.items}>
+            <DishList citySlug={city.slug} />
+          </div>
         </div>
-      </div>
-    </PageTemplate>
+      </PageTemplate>
+    </>
   );
 }
 
